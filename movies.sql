20a459cccccd9c81f35b4c67ef4f5a9ee2ea04f5
febinvarghese <60997978+febinvarghese@users.noreply.github.com>
2021-09-04 14:17:51 +0530

    Add files via upload

---
+++ movies.sql
@@ -0,0 +1,18 @@
+/*
+SQLyog Community Edition- MySQL GUI
+MySQL - 5.7.19 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `intresting movies` (
+	`movie name` varchar (135),
+	`lead actor` varchar (135),
+	`lead actress` varchar (135),
+	`year of release` double ,
+	`director name` varchar (135)
+); 
+insert into `intresting movies` (`movie name`, `lead actor`, `lead actress`, `year of release`, `director name`) values('Django Unchained','Kerry Washington','Jamie Foxx','2021','Quentin Tarantino');
+insert into `intresting movies` (`movie name`, `lead actor`, `lead actress`, `year of release`, `director name`) values('Forrest Gump','Tom Hanks','Robin Wright','1994','Rober Zemeckis');
+insert into `intresting movies` (`movie name`, `lead actor`, `lead actress`, `year of release`, `director name`) values('Interstellar','Matthew McConaughe','Anne Hathaway','2014','Christopher Nolan');
+insert into `intresting movies` (`movie name`, `lead actor`, `lead actress`, `year of release`, `director name`) values('\'Inception','Leonard Dicaprio','Marion Cotillard','2010','Christopher Nolan');
